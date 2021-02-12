978c4ed7292c125a24c75419e7561fbffe2cdc5b
Jean <rubin.jean98@gmail.com>
2021-02-12 20:05:47 +0100

    Added linear kernel

--- utils/kernels.py
+++ utils/kernels.py
@@ -2,6 +2,15 @@
 
 import numpy as np
 
+def linear_kernel(X, Y):
+    """
+    Return the Gram matrix K(X,Y) with K being the linear kernel
+    """
+    return np.sum(X[:,np.newaxis] * Y[np.newaxis], axis = 2)
+
 def gaussian_kernel(X, Y, sigma = 1):
-    dist = np.sum((X[:,np.newaxis] - X[np.newaxis])**2, axis = 2)
+    """
+    Return the Gram matrix K(X,Y) with K being the gaussian kernel
+    """
+    dist = np.sum((X[:,np.newaxis] - Y[np.newaxis])**2, axis = 2)
     return np.exp(-0.5*dist/sigma**2)
