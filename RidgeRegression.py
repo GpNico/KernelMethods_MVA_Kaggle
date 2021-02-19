2701bbb2a9b224ee35dd1496ccb5627f49d88ab0
unknown <nicolas.guerin@polytechnique.edu>
2021-02-19 18:29:45 +0100

    Linear Ridge Regression

---
+++ RidgeRegression.py
@@ -0,0 +1,24 @@
+# RidgeRegression.py
+
+import numpy as np
+import scipy.linalg
+
+class RidgeRegression:
+    """
+    Classical ridge regression 
+    """
+
+    def __init__(self, alpha):
+        self.alpha = alpha
+        self.coef = 0
+        self.X = np.zeros((1,1))
+    
+    def fit(self, X, y):
+        n, d = X.shape
+        M = np.matmul(np.transpose(X), X) + self.alpha * n * np.eye(d)
+        self.wRR = scipy.linalg.solve(M, np.matmul(np.transpose(X), y))
+        self.X = X
+        return
+
+    def predict(self, X):
+        return np.matmul(np.transpose(X), X) @ self.wRR
