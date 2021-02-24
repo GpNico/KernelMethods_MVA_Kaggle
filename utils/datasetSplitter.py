103177d39c18a1047727a51e9c4c1c8c10595d44
Jean <rubin.jean98@gmail.com>
2021-02-24 14:26:10 +0100

    Added dataset splitting utility functions

---
+++ utils/datasetSplitter.py
@@ -0,0 +1,47 @@
+# datasetSplitter.py
+
+import numpy as np
+
+
+class TrainTestSplitter:
+    """
+    Classical dataset splitting
+    """
+    def __init__(self, seed = 42):
+        self.seed = seed
+        self.train_idx: np.array
+        self.test_idx: np.array
+    
+    def generate_idx(self, X, percent):
+        np.random.seed(self.seed)
+        nrow = X.shape[0]
+        train_size = int(percent * nrow)
+
+        shuffled_index = np.arange(nrow)
+        np.random.shuffle(shuffled_index)
+
+        self.train_idx = shuffled_index[:train_size]
+        self.test_idx = shuffled_index[train_size:]
+    
+    def split(self, X):
+        return X[self.train_idx], X[self.test_idx]
+
+
+class BalancedTrainTestSplitter(TrainTestSplitter):
+    """
+    Dataset splitting by respecting labels proportion
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def generate_idx(self, y, percent):
+        np.random.seed(self.seed)
+        idx0, idx1 = np.where(y == 0)[0], np.where(y == 1)[0]
+
+        np.random.shuffle(idx0)
+        np.random.shuffle(idx1)
+
+        train_size0, train_size1 = int(percent*len(idx0)), int(percent*len(idx1))
+
+        self.train_idx = np.hstack([idx0[:train_size0], idx1[:train_size1]])
+        self.test_idx = np.hstack([idx0[train_size0:], idx1[train_size1:]])
