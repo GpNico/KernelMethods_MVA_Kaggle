1bf99095acccf63a64bfb1c6b080af813720bdbc
Jean <rubin.jean98@gmail.com>
2021-02-24 15:46:08 +0100

    Added preprocess script

---
+++ utils/datasetPreprocess.py
@@ -0,0 +1,12 @@
+# datasetPreprocess.py
+# Preprocessing should should have a fit and a transform step
+
+def convert_numpy(df):
+    return df.to_numpy()
+
+def drop_columns(df, ids):
+    return df.drop(ids, axis = 1)
+
+def recode(df):
+    df["Bound"] = 2*df["Bound"] - 1
+    return df
