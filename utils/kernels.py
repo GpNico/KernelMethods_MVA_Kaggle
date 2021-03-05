9f827376931ec66196719a32328b763fe8be12ba
Jean <rubin.jean98@gmail.com>
2021-03-05 23:18:19 +0100

    Fixed polynomial kernel according to litterature

--- utils/kernels.py
+++ utils/kernels.py
@@ -14,7 +14,7 @@ def polynomial_kernel(X, Y, exponent = 2):
     """
     Return the Gram matrix K(X,Y) with K being the polynomial kernel
     """
-    return np.sum((X[:,np.newaxis]*Y[np.newaxis])**exponent, axis = 2)
+    return np.sum((X[:,np.newaxis]*Y[np.newaxis] + 1)**exponent, axis = 2)
 
 def gaussian_kernel(X, Y, gamma = 1):
     """
