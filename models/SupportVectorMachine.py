19578aa2ce9db1f337d3fa7195767620b31f3994
unknown <nicolas.guerin@polytechnique.edu>
2021-03-07 16:57:59 +0100

    SVM

---
+++ models/SupportVectorMachine.py
@@ -0,0 +1,49 @@
+# SupportVectorMachine.py
+
+import numpy as np
+import scipy.linalg
+from cvxopt import matrix, solvers
+
+class SVM:
+    """
+        Support Vector Machine 
+    """
+
+    def __init__(self, kernel, alpha):
+        self.alpha = alpha
+        self.coef = 0
+        self.X = np.zeros((1,1))
+        self.kernel = kernel
+    
+    def fit(self, X, y):
+        n, d = X.shape
+        K = self.kernel(X, X)
+        
+        Q = matrix(K)
+        p = matrix(y.astype('float'))
+        
+        G = matrix(np.concatenate((np.diag(y).astype('float'), np.diag(-y).astype('float')),axis=0))
+        C = 1./(2*n*self.alpha)
+        h = matrix(np.concatenate((C*np.ones(n), np.zeros(n)),axis =0))
+        
+        solvers.options['show_progress'] = False
+        sol = solvers.qp(Q,p,G,h)
+        
+        self.coef = np.array(sol['x']).flatten()
+        
+        return
+
+    def predict(self, X):
+        return np.dot(self.kernel(X, self.X), self.coef)
+        
+class SVMClassification(SVM):
+    """
+    SVM classification
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def predict(self, X):
+        return np.sign(super().predict(X))
+        
